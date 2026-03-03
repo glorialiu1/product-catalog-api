d96f87900ca2ec0f5650b85c35085d27ade2a917
Gloria Liu <gloria_liu99@hotmail.se>
2026-03-03 19:18:32 +0100

    Updated Colour class.

--- src/main/java/com/github/glorialiu1/product_catalog_api/entity/Colour.java
+++ src/main/java/com/github/glorialiu1/product_catalog_api/entity/Colour.java
@@ -6,7 +6,30 @@ import jakarta.persistence.*;
 @Table(name = "colour")
 public class Colour {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // Auto generates ID for this entity.
+    private Long id;
+
+    @Column(nullable = false, unique = true)        // name is required and unique.
+    private String name;
+
     public Colour() {
-        
+
+    }
+
+    public Colour(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
