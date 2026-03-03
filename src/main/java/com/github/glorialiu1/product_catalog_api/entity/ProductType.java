a95b281ed443ceb54ed7687816e9a70905c03d8d
Gloria Liu <gloria_liu99@hotmail.se>
2026-03-03 19:17:53 +0100

    Updated ProductType class.

--- src/main/java/com/github/glorialiu1/product_catalog_api/entity/ProductType.java
+++ src/main/java/com/github/glorialiu1/product_catalog_api/entity/ProductType.java
@@ -6,7 +6,30 @@ import jakarta.persistence.*;
 @Table(name = "product_type")
 public class ProductType {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto generate ID for this entity.
+    private Long id;
+
+    @Column(nullable = false, unique = true)        // name is required and unique.
+    private String name;
+
     public ProductType() {
-        
+
+    }
+
+    public ProductType(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
